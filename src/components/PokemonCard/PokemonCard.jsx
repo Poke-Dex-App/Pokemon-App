8512be10d4d89c85802c2698df1b8bfa9d0372e8
Armando <argradi2003@gmail.com>
2026-03-23 11:29:14 +0100

    feat: first pokemon details added

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -1,20 +1,24 @@
+import { Link } from 'react-router-dom';
 import './PokemonCard.css'
 
 function PokemonCard (props) {
 
     const primType = props.poke.types[0];
+    //console.log(props.poke)
 
     return(
-        <div className={`poke-card ${primType}`}>
-            <p>{props.poke.latest_index}</p>
-            <img src={props.poke.sprites.front}/>
-            <h2>{props.poke.name}</h2>
-            <div className='types'>
-                {props.poke.types.map((type) => {
-                    return <p className={`type-p ${type}`} key={type}>{type}</p>
-                })}
+        <Link to={`/pokemons/${props.poke.id}`}>
+            <div className={`poke-card ${primType}`}>
+                <p>{props.poke.latest_index}</p>
+                <img src={props.poke.sprites.front}/>
+                <h2>{props.poke.name}</h2>
+                <div className='types'>
+                    {props.poke.types.map((type, i) => {
+                        return <p className={`type-p ${type}`} key={i}>{type}</p>
+                    })}
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
--- src/pages/PokeListDetails/PokeListDetails.jsx
+++ src/pages/PokeListDetails/PokeListDetails.jsx
@@ -1,7 +1,39 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+
 function PokeDetailsPage (){
 
+    const [pokemon, setPokemon] = useState({})
+
+    const {pokeId} = useParams()
+
+    const baseURL = 'https://pokemon-app-ca105-default-rtdb.europe-west1.firebasedatabase.app/resource'
+
+    const getPokemon = () => {
+        axios
+            .get(`${baseURL}.json?orderBy="id"&equalTo="${pokeId}"`)
+            .then((poke) => {
+                const elPokemon = Object.values(poke.data)[0];
+                setPokemon(elPokemon)
+            })
+            .catch((error) => console.log(error))
+    }
+
+    useEffect(() => {
+        getPokemon()
+    }, [pokeId])
+
+    if (!pokemon) {
+        return <p>Cargando datos del Pokémon...</p>;
+    }
+
     return(
-        <></>
+        <>
+            <h1>Poke Details</h1>
+            <p>{pokemon.name}</p>
+            <p>{pokeId}</p>
+        </>
     )
 }
 
